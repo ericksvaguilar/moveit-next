e1c190c72c34357dbbc2cc930c71c7eec4cf3dcf
Erick Aguilar <ericksvaguilar@gmail.com>
2021-02-23 10:11:02 -0300

    finish Profile module

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,15 @@
+import styles from '../styles/components/Profile.module.css'
+
 export function Profile() {
   return (
-    <div>
+    <div className={styles.profileContainer}>
       <img src='https://github.com/ericksvaguilar.png' alt='Erick Aguilar' />
       <div>
         <strong>Erick Aguilar</strong>
-        <p>Level 1</p>
+        <p>
+          <img src='icons/level.svg' alt='Level' />
+          Level 1
+        </p>
       </div>
     </div>
   )
