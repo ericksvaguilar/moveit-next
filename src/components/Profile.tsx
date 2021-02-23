5ba33944e23d023caedf993701520648f9e91781
Erick Aguilar <ericksvaguilar@gmail.com>
2021-02-23 09:58:08 -0300

    create Profile module

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src='https://github.com/ericksvaguilar.png' alt='Erick Aguilar' />
+      <div>
+        <strong>Erick Aguilar</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
