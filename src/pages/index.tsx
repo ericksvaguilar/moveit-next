1d7562ac03a00ed29ad3d50324ce82a79a9ff613
Erick Aguilar <ericksvaguilar@gmail.com>
2021-02-24 10:36:37 -0300

    feat: import ChallengeBox to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import { Profile } from '../components/Profile'
 import Head from 'next/head'
 
 import styles from '../styles/pages/Home.module.css'
+import { ChallengeBox } from '../components/ChallengeBox'
 
 export default function Home() {
   return (
@@ -25,7 +26,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
