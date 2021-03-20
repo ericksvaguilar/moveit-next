306c6b321df186e88ae3264ae408b90ca9804800
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-20 09:11:56 -0300

    fix: add return type to countdownprovider func

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -24,10 +24,12 @@ export const CountdownContext = createContext({} as CountdownContextData);
 
 let countdownTimeout: NodeJS.Timeout;
 
-export function CountdownProvider({ children }: CountdownProviderProps) {
+export function CountdownProvider({
+  children,
+}: CountdownProviderProps): JSX.Element {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(25 * 60); // Start countdown with 25min, but in seconds
+  const [time, setTime] = useState(0.1 * 60); // Start countdown with 25min, but in seconds
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -53,7 +55,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(25 * 60);
+    setTime(0.1 * 60);
     setHasFinished(false);
   }
 
