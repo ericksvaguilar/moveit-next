02bd18aa1533cdbcf19c3d64938da3af823986ae
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-20 10:07:48 -0300

    fix: add return type to mydocument class

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  render() {
+  render(): JSX.Element {
     return (
       <Html>
         <Head>
