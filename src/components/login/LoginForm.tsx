4c85eb544dee2f94050beb512cd9668358fc1af8
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-26 15:49:08 -0300

    feat: import login form styles

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import styles from '../../styles/components/login/LoginForm.module.css';
 
 const LoginForm = (): JSX.Element => {
   return (
-    <form>
-      <input type="text" placeholder="Digite seu username" />
-      <button type="submit">
-        <img src="img/arrow-right.svg" alt="arrow" />
-      </button>
-    </form>
+    <div className={styles.loginFormContainer}>
+      <form>
+        <input type="text" placeholder="Digite seu username" />
+        <button type="submit">
+          <img src="img/arrow-right.svg" alt="arrow" />
+        </button>
+      </form>
+    </div>
   );
 };
 
