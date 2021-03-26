63836d9e05c772c6dc3d892941e931c825cf4a8c
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-26 14:54:29 -0300

    add logo and symbol images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styles from '../styles/pages/Login.module.css';
 
 const Login = (): JSX.Element => {
-  return <h1>Ola</h1>;
+  return (
+    <div className={styles.loginContainer}>
+      <div>
+        <img className={styles.simbolo} src="img/simbolo.svg" alt="simbolo" />
+      </div>
+
+      <div>
+        <img className={styles.logo} src="img/logo.svg" alt="logo" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
