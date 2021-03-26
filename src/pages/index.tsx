6560e91db4f560e04519b38df748b10751202121
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-26 14:29:35 -0300

    feat: import Link from next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Login = (): JSX.Element => {
-  return <h1>Login</h1>;
+  return (
+    <h1>
+      <Link href="/challenge">Challenge</Link>
+    </h1>
+  );
 };
 
 export default Login;
